2237e56da863b95ee4da0b66f4f2088cc4719c22
ta-ka-13 <edll5505d@yahoo.co.jp>
2020-12-18 10:23:24 +0900

    css_変更

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -3,12 +3,21 @@
 
   <head>
   <meta charset="UTF-8">
+  <script>
+    window.onload = function(){
+      setInterval(function(){
+        var dd = new Date();
+        document.getElementById("T1").innerHTML = dd.toLocaleString();
+      }, 1000);
+    }
+  </script>
   <link rel="stylesheet" href="{{ asset('css/style.css') }}">
   </head>
 
 
   <body>
 @include("parts.header")
+<!-- <div id="T1"></div> -->
 
     <div main>
     ハローワールド
