4b59870a3f882849be99dd39390bd6e643134691
ta-ka-13 <edll5505d@yahoo.co.jp>
2021-01-18 22:10:17 +0900

    add_myabout

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
+<script>
+    window.onload = function(){
+      setInterval(function(){
+        var dd = new Date();
+        document.getElementById("T1").innerHTML = dd.toLocaleString();
+      }, 1000);
+    }
+  </script>
 <link rel="stylesheet" href="{{  asset('css/style.css') }}" />
 </head>
 <body>
--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -1,6 +1,7 @@
 
+
 <header>
-  <h1>Laravel Web page</h1>
+  <h1>Laravel Web Page</h1>
 
 現在時刻:<T id="T1"></T>
   @include("parts.nav")
